400cf1361050c2cff6cb48d147bb0747c2633dbb
Yann Guillon <yann@dockdev.com>
2022-08-09 15:01:46 +0200

    add transaction

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
+const qs = require('qs')
 
 const customParams = {
   url: ['url'],
   selector: ['selector'],
   challenge: ['challenge'],
+  recipient:  ['recipient'],
+  walletID:  ['walletID'],
   endpoint: false
 }
 
 const createRequest = (input, callback) => {
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const serviceURL = "${process.env.BROWSERLESS_URL}/scrape?stealth"
+  const walletID = validator.validated.data.walletID
+  const recipient = validator.validated.data.recipient
+  const serviceURL = `${process.env.BROWSERLESS_URL}/scrape?stealth`
+  const loginURL = `${process.env.VUE_APP_VENLY_LOGIN_URL}/auth/realms/Arkane/protocol/openid-connect/token`
+  const venlyWalletURL = `${process.env.VUE_APP_VENLY_API_URL}/api/wallets/${walletID}`
+  const venlyTransferServiceURL = `${process.env.VUE_APP_VENLY_API_URL}/api/transactions/execute`
+  const pincode = '1234'
   const url = validator.validated.data.url
   const challenge = validator.validated.data.challenge
   const elements = [{ selector: validator.validated.data.selector }]
@@ -20,6 +29,13 @@ const createRequest = (input, callback) => {
     elements
   }
 
+  const venlyConfig = {
+    method: 'get',
+    url: venlyWalletURL,
+    data: {},
+    timeout: 20000
+  }
+
   const config = {
     method: 'post',
     url: serviceURL,
@@ -27,18 +43,75 @@ const createRequest = (input, callback) => {
     timeout: 20000
   }
 
+  const login_config = {
+    method: 'post',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    url: loginURL,
+    data: qs.stringify({
+      'grant_type': 'client_credentials',
+      'client_id': process.env.VUE_APP_VENLY_CLIENT_ID,
+      'client_secret': process.env.VUE_APP_SECRET_ID
+    }),
+    timeout: 20000
+  }
   Requester.request(config)
     .then(response => {
-      console.log(response)
       let match = false
       if (response.data && response.data.data[0] && response.data.data[0].results[0]) {
         match = response.data.data[0].results[0].html.includes(challenge)
+        if (match) {
+          Requester.request(login_config)
+              .then(response => {
+                const token = response.data.access_token
+                const config2 = {
+                  method: 'get',
+                  headers: {'content-type': 'application/json', 'Authorization': `Bearer ${token}`},
+                  url: venlyWalletURL,
+                  data: {},
+                  timeout: 20000
+                }
+                Requester.request(config2)
+                    .then(response => {
+                      const balance = response.data.result.balance.balance - 0.1
+
+                      const params =
+                          {pincode,
+                          "transactionRequest": {
+                        "type" : "TRANSFER",
+                            "secretType" : "MATIC",
+                            "walletId" : walletID,
+                            "to" : recipient,
+                            "value": balance
+                      }
+                    }
+                      const config3 = {
+                        method: 'post',
+                        headers: {'content-type': 'application/json', 'Authorization': `Bearer ${token}`},
+                        url: venlyTransferServiceURL,
+                        data: params
+                          ,
+                        timeout: 20000
+                      }
+                      Requester.request(config3)
+                          .then(response => {
+                           const hash = response.data.result.transactionHash
+                            callback(response.status, Requester.success(jobRunID, {data: {result: hash}}))
+                          })
+                          .catch(error => {
+                            callback(200, Requester.success(jobRunID, {data: {result: false}}))
+                          })
+                    })
+                    .catch(error => {
+                      callback(200, Requester.success(jobRunID, {data: {result: false}}))
+                    })
+              }).catch(error => {
+            callback(200, Requester.success(jobRunID, {data: {result: false}}))
+          })
+        }
       }
-      callback(response.status, Requester.success(jobRunID, {data: {result: match}}))
-    })
-    .catch(error => {
-      callback(200, Requester.success(jobRunID, {data: {result: false}}))
-    })
+    }).catch(error => {
+    callback(200, Requester.success(jobRunID, {data: {result: false}}))
+  })
 }
 
 module.exports.createRequest = createRequest
